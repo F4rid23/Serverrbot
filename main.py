2a38c1be48a4c512af7cace94dfcffc6569303d6
F4rid23 <farid23092001@mail.ru>
2025-05-08 23:39:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,32 @@ dp = Dispatcher(bot)
 register_handlers(dp)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
+from config import BOT_TOKEN, WEBHOOK_URL, WEBAPP_HOST, WEBAPP_PORT
+from handlers import register_handlers
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+register_handlers(dp)
+
+# Вебхук настройки
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path='',
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
